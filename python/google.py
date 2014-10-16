94cefabb6e24dfd0457d6c7bb6d326d0eaa529e6
RandomK <gyqmstc@sina.com>
2014-10-16 12:23:27 +0800

    bugfix

--- python/google.py
+++ python/google.py
@@ -101,7 +101,7 @@ class GoogleFetch(object):
         content = content.replace('=\'/images/', '=\'https://wen.lu/images/')
         content = content.replace('url(/images/', 'url(https://wen.lu/images/')
         content = content.replace('//ssl.gstatic.com', 'https://www.glgoo.com/gstatic/ssl')
-        content = re.sub(r'onmousedown=".+?" ', '', content)
+        content = re.sub(r'onmousedown=".+?"', '', content)
         content = re.sub(r'(https?:)?//fonts\.googleapis\.com', 'https://fonts.lug.ustc.edu.cn', content)
         content = re.sub(r'(https?:)?//www\.google\.com', '//%s' % host, content)
         content = re.sub(r'(https?:)?//%s' % self.google_ip.replace('.', r'\.'), '//%s' % host, content)
