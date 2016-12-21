71bbd7bee462e3406befb7d1b9f00ad861599277
RandomK <gyqmstc@sina.com>
2016-12-21 18:37:50 +0800

    update sshrcheck.sh

--- shell/sshrcheck.sh
+++ shell/sshrcheck.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-while true; do
-    nc -nvz 192.168.0.110 2222 && nc -nvz 192.168.0.110 3389
-    if [ $? -ne 0 ]; then
-        pgrep -u pi -x -a sshd | awk '$3=="pi" {print $1}' | xargs kill
-    fi
-    sleep 60
-done
+nc -nvz 192.168.0.110 2222 && nc -nvz 192.168.0.110 3389
+if [ $? -ne 0 ]; then
+    pgrep -u pi -x -a sshd | awk '$3=="pi" {print $1}' | xargs -r kill
+fi
