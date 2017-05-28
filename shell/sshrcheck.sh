4376377904c2edb11f52373a4fa23822b85c41bc
RandomK <gyqmstc@sina.com>
2017-05-29 01:58:48 +0800

    update sshrcheck.sh

--- shell/sshrcheck.sh
+++ shell/sshrcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nc -nvz 192.168.0.110 2222 && nc -nvz 192.168.0.110 3389
+nc -nvz 192.168.0.110 2222
 if [ $? -ne 0 ]; then
     pgrep -u pi -x -a sshd | awk '$3=="pi" {print $1}' | xargs -r kill
 fi
