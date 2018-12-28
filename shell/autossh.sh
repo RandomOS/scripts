1b7e94817db71012278726eb45a3a8c4f2fb5ca3
RandomK <gyqmstc@sina.com>
2018-12-28 11:47:17 +0800

    update autossh.sh

--- shell/autossh.sh
+++ shell/autossh.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 while true; do
-    if [ ! -n "$(ps aux | grep qnfNT | grep -v grep)" ]; then
-        ssh -qnfNT -R 443:127.0.0.1:22 root@192.168.0.110 -p 22
+    if [ ! -n "$(pgrep -f 'ssh -qnfNT -R 127.0.0.1:22222:192.168.0.110:22')" ]; then
+        ssh -qnfNT -R 127.0.0.1:22222:192.168.0.110:22 \
+            -o ServerAliveInterval=15 \
+            -o ExitOnForwardFailure=yes \
+        root@192.168.0.110
     fi
     sleep 5
 done
+
--- shell/sshrcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-nc -nvz 192.168.0.110 2222
-if [ $? -ne 0 ]; then
-    pgrep -u pi -x -a sshd | awk '$3=="pi" {print $1}' | xargs -r kill
-fi
