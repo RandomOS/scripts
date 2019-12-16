800a1ca1a7f1cef77eab440d13218c47e956703e
RandomK <gyqmstc@sina.com>
2019-12-17 01:18:16 +0800

    add alpinelab.sh

---
+++ shell/alpinelab.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+apk update && apk add --no-cache bash bash-completion curl vim tzdata
+
+curl -4sk -o /etc/inputrc https://cdn.jsdelivr.net/gh/randomos/dockerfiles@master/alpine-lab/etc/inputrc
+curl -4sk -o /root/.bashrc https://cdn.jsdelivr.net/gh/randomos/dockerfiles@master/alpine-lab/root/.bashrc
+curl -4sk -o /root/.vimrc https://cdn.jsdelivr.net/gh/randomos/dockerfiles@master/alpine-lab/root/.vimrc
