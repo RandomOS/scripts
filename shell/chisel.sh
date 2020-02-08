48d831303f33df2ea412fe77b21bd216f6381f81
RandomK <gyqmstc@sina.com>
2020-02-08 12:47:11 +0800

    add chisel.sh

---
+++ shell/chisel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# wget -q -O - https://url.cn/5YqbghT | sh
+
+export PATH="$PATH:."
+export WORK_DIR="/tmp"
+
+cd $WORK_DIR
+
+if [ ! -x $WORK_DIR/chisel ]; then
+    wget -q -O chisel.gz https://github.com/jpillora/chisel/releases/download/1.3.0/chisel_linux_amd64.gz
+    gzip -d chisel.gz && chmod +x chisel
+fi
+
+if [ ! -x $WORK_DIR/cloudflared ]; then
+    wget -q -O cloudflared.tgz https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz
+    tar xf cloudflared.tgz
+fi
+
+pkill -x chisel
+pkill -x cloudflared
+truncate -s 0 $WORK_DIR/cloudflared.log
+(chisel server --host 127.0.0.1 --port 54321 --auth chisel:chisel --socks5 >/dev/null 2>&1 &)
+(cloudflared tunnel --no-autoupdate --url http://127.0.0.1:54321 --logfile $WORK_DIR/cloudflared.log >/dev/null 2>&1 &)
