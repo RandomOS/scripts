56a3ed181abaa798ad8fe9119b294879ae9dc6c2
RandomK <gyqmstc@sina.com>
2020-03-16 00:57:39 +0800

    add install-docker.sh

---
+++ shell/install-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# wget -q -O - http://t.cn/A6zKRKsu | sh
+# wget -q -O - https://gitee.com/randomk/scripts/raw/master/shell/install-docker.sh | sh
+
+if [ "$(whoami)" != "root" ]; then
+    sudo -u root $0
+    exit
+fi
+
+if [ -x "$(command -v docker)" ]; then
+    echo "docker already installed"
+    exit
+fi
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
+mkdir -p /etc/docker
+cat << EOF > /etc/docker/daemon.json
+{
+    "registry-mirrors": ["https://dockerhub.azk8s.cn"],
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "1m",
+        "max-file": "1"
+    }
+}
+EOF
+
+service docker restart
