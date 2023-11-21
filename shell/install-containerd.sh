f0db32a028ccc80bf5497ee8c914b2b5fba9d84e
RandomK <gyqmstc@sina.com>
2023-11-21 16:02:19 +0800

    update install-nerdctl.sh

--- shell/install-containerd.sh
+++ shell/install-containerd.sh
@@ -2,9 +2,9 @@
 
 # wget -q -O - https://jihulab.com/RandomK/scripts/raw/master/shell/install-containerd.sh | sh
 
-CONTAINERD_VERSION="1.7.7"
-NERDCTL_VERSION="1.6.2"
-RUNC_VERSION="1.1.9"
+CONTAINERD_VERSION="1.7.9"
+NERDCTL_VERSION="1.7.0"
+RUNC_VERSION="1.1.10"
 CNI_VERSION="1.3.0"
 
 if [ "$(whoami)" != "root" ]; then
@@ -19,18 +19,18 @@ fi
 rm -rf /tmp/install \
     && mkdir -p /tmp/install \
     && cd /tmp/install \
-    && wget -q -O data.tar.gz https://ghproxy.com/https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-amd64.tar.gz \
+    && wget -q -O data.tar.gz https://mirror.ghproxy.com/https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-amd64.tar.gz \
     && tar -C /usr/local -xf data.tar.gz \
-    && wget -q -O data.tar.gz https://ghproxy.com/https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION}-linux-amd64.tar.gz \
+    && wget -q -O data.tar.gz https://mirror.ghproxy.com/https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION}-linux-amd64.tar.gz \
     && tar -C /usr/local/bin -xf data.tar.gz \
     && ln -sf /usr/local/bin/nerdctl /usr/local/bin/docker \
-    && wget -q -O runc.amd64 https://ghproxy.com/https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.amd64 \
+    && wget -q -O runc.amd64 https://mirror.ghproxy.com/https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.amd64 \
     && install -m 755 runc.amd64 /usr/local/sbin/runc \
     && mkdir -p /opt/cni/bin \
-    && wget -q -O data.tar.gz https://ghproxy.com/https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz \
+    && wget -q -O data.tar.gz https://mirror.ghproxy.com/https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz \
     && tar -C /opt/cni/bin -xf data.tar.gz \
     && mkdir -p /usr/local/lib/systemd/system \
-    && wget -q -O /usr/local/lib/systemd/system/containerd.service https://ghproxy.com/https://raw.githubusercontent.com/containerd/containerd/main/containerd.service \
+    && wget -q -O /usr/local/lib/systemd/system/containerd.service https://mirror.ghproxy.com/https://raw.githubusercontent.com/containerd/containerd/main/containerd.service \
     && cd /tmp \
     && rm -rf /tmp/install
 
--- shell/install-nerdctl.sh
+++ shell/install-nerdctl.sh
@@ -2,7 +2,7 @@
 
 # wget -q -O - https://jihulab.com/RandomK/scripts/raw/master/shell/install-nerdctl.sh | sh
 
-NERDCTL_VERSION="1.6.2"
+NERDCTL_VERSION="1.7.0"
 
 if [ "$(whoami)" != "root" ]; then
     exit
@@ -16,7 +16,7 @@ fi
 rm -rf /tmp/install \
     && mkdir -p /tmp/install \
     && cd /tmp/install \
-    && wget -q -O data.tar.gz https://ghproxy.com/https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz \
+    && wget -q -O data.tar.gz https://mirror.ghproxy.com/https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz \
     && tar -C /usr/local -xf data.tar.gz \
     && ln -sf /usr/local/bin/nerdctl /usr/local/bin/docker \
     && cd /tmp \
