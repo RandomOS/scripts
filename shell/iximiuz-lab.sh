6e2344e864c06e7afc5cc661a9687ebe5dfe84cc
RandomK <gyqmstc@sina.com>
2025-02-13 16:30:06 +0800

    update iximiuz-lab.sh

--- shell/iximiuz-lab.sh
+++ shell/iximiuz-lab.sh
@@ -5,7 +5,7 @@
 STARSHIP_VERSION="1.8.0"
 
 install_pkg() {
-    apt-get install -y tmux fish >/dev/null 2>&1
+    apt-get install -y fish lftp tmux tmuxp >/dev/null 2>&1
 
     mkdir -p /tmp/install \
         && cd /tmp/install \
@@ -14,7 +14,7 @@ install_pkg() {
         && cp -f starship /usr/local/bin/
 }
 
-init_config() {
+init_root_config() {
     curl -4sk -o /root/.bashrc https://gist.githubusercontent.com/RandomOS/09ad75edaf5e27548f7314c11cb9d30c/raw/f7209c5f8c6641f155e529a80e887573e15e8b2c/.bashrc
     curl -4sk -o /root/.vimrc https://cdn.jsdelivr.net/gh/randomos/dockerfiles@master/alpine-lab/root/.vimrc
     curl -4sk -o /root/.tmux.conf https://cdn.jsdelivr.net/gh/randomos/dockerfiles@master/alpine-lab/root/.tmux.conf
@@ -31,11 +31,42 @@ alias mps='ps -u $USER -f f'
 starship init fish | source
 EOF
 
+    mkdir -p /root/.config/tmuxp
+    cat << 'EOF' > /root/.config/tmuxp/dev.yaml
+session_name: dev
+start_directory: /dev/shm
+windows:
+  - window_name: fish
+    focus: true
+    panes:
+    - shell_command:
+      - fish
+      - clear
+      focus: true
+  - window_name: bash
+    panes:
+    - shell_command:
+      - clear
+      focus: true
+  - window_name: bash
+    panes:
+    - shell_command:
+      - clear
+      focus: true
+EOF
+}
+
+init_user_config() {
     mkdir -p /home/laborant/.config/fish
     cp -f /root/.config/fish/config.fish /home/laborant/.config/fish/
     cp -f /root/.vimrc /home/laborant/
     cp -f /root/.tmux.conf /home/laborant/
     chown -R laborant:laborant /home/laborant
+}
+
+init_config() {
+    init_root_config
+    init_user_config
 }
 
 init_system() {
