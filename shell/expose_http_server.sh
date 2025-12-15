043681598afb76f9ad0ac64c9ef91a6156a3dac2
RandomK <gyqmstc@sina.com>
2025-12-15 14:27:39 +0800

    add expose_http_server.sh

---
+++ shell/expose_http_server.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# curl -fsSL https://raw.githubusercontent.com/RandomOS/scripts/master/shell/expose_http_server.sh | sudo bash
+
+set -euo pipefail
+[[ ${DEBUG:-0} == 1 ]] && set -x
+
+GITHUB_URL="https://github.com"
+MINISERVE_VERSION="0.32.0"
+
+if [[ -x $(command -v miniserve) ]]; then
+    if [[ $(miniserve -V | cut -d ' ' -f 2) != ${MINISERVE_VERSION} ]]; then
+        rm -rf /tmp/install \
+            && mkdir -p /tmp/install \
+            && cd /tmp/install \
+            && wget -q -O miniserve ${GITHUB_URL}/svenstaro/miniserve/releases/download/v${MINISERVE_VERSION}/miniserve-${MINISERVE_VERSION}-x86_64-unknown-linux-musl \
+            && chmod +x miniserve \
+            && sudo cp -f miniserve /usr/local/bin/ \
+            && cd /tmp \
+            && rm -rf /tmp/install
+
+        if [[ $? -ne 0 ]]; then
+            echo "[error] download failed"
+            exit
+        fi
+    fi
+fi
+
+mkdir -p /tmp/www
+killall miniserve 2>&1
+(miniserve -D -p 8080 /tmp/www >/dev/null 2>&1 &)
+
+ssh -o StrictHostKeyChecking=no -t -R 80:localhost:8080 proxy.tunnl.gg
