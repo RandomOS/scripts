b850dded07742cffe95561deefc71c5dabb38faf
RandomK <randomk@foxmail.com>
2026-07-29 16:32:26 +0800

    refactor: improve socket handling and connection lifecycle management

--- python/pytunnel.py
+++ python/pytunnel.py
@@ -56,94 +56,140 @@ def wraptlv(tag, value):
     return data
 
 
+def recvall(sock, size):
+    """Read exactly size bytes. Return None on EOF, '' when size is 0."""
+    data = ''
+    while len(data) < size:
+        buf = sock.recv(size - len(data))
+        if not buf:
+            return None
+        data += buf
+    return data
+
+
 def recvtlv(sock):
-    data = sock.recv(1)
-    if not data:
-        return data
+    """Read one frame. Return None on EOF or desync, '' for an empty frame."""
+    data = recvall(sock, 1)
+    if data is None:
+        return None
     tag = struct.unpack('B', data)[0]
     if tag != TAG:
-        return ''
-    data = sock.recv(2)
-    if not data:
-        return data
+        # the stream is out of sync, there is no way to resynchronise
+        logger.error('bad tag %d, expected %d', tag, TAG)
+        return None
+    data = recvall(sock, 2)
+    if data is None:
+        return None
     length = struct.unpack('!H', data)[0]
-    data = ''
-    size = length
-    count = 0
-    while size > 0:
-        buf = sock.recv(size)
-        data += buf
-        size = length - len(data)
-        count += 1
-        if count > 5 and size > 0:
-            return ''
-    return data
+    return recvall(sock, length)
+
+
+class Link(object):
+    """Shared teardown state for the two relay threads of one connection."""
+
+    def __init__(self, sock_a, sock_b):
+        self.socks = (sock_a, sock_b)
+        self.lock = threading.Lock()
+        self.done = 0
+
+    def finish(self, abort):
+        if abort:
+            # the connection is broken, kick the peer thread out of its read
+            for sock in self.socks:
+                try:
+                    sock.shutdown(socket.SHUT_RDWR)
+                except socket.error:
+                    pass
+        with self.lock:
+            self.done += 1
+            last = self.done == len(self.socks)
+        if last:
+            for sock in self.socks:
+                try:
+                    sock.close()
+                except socket.error:
+                    pass
 
 
 class SendEncrypt(threading.Thread):
 
-    def __init__(self, source_sock, target_sock, key):
+    def __init__(self, source_sock, target_sock, key, link, source_addr, target_addr):
         super(SendEncrypt, self).__init__()
         self.source_sock = source_sock
         self.target_sock = target_sock
         self.key = key
-        self.source_addr = self.source_sock.getpeername()
-        self.target_addr = self.target_sock.getpeername()
+        self.link = link
+        self.source_addr = source_addr
+        self.target_addr = target_addr
 
     def run(self):
+        abort = False
         while True:
             try:
                 data = self.source_sock.recv(4096)
                 if not data:
                     break
                 logger.debug('read  %04i from %s:%d', len(data), self.source_addr[0], self.source_addr[1])
-                sent = self.target_sock.send(wraptlv(TAG, encrypt(data, self.key)))
-                logger.debug('write %04i to   %s:%d', sent, self.target_addr[0], self.target_addr[1])
+                frame = wraptlv(TAG, encrypt(data, self.key))
+                self.target_sock.sendall(frame)
+                logger.debug('write %04i to   %s:%d', len(frame), self.target_addr[0], self.target_addr[1])
             except socket.error as e:
                 logger.error('socket error, e: %s', e)
+                abort = True
                 break
             except Exception as e:
                 logger.error('unknown error, e: %s', e)
+                abort = True
                 break
         logger.debug('connection %s:%d is closed.', self.source_addr[0], self.source_addr[1])
-        try:
-            self.source_sock.shutdown(socket.SHUT_RDWR)
-            self.target_sock.shutdown(socket.SHUT_RDWR)
-        except socket.error as e:
-            pass
+        if not abort:
+            # propagate the half close so the peer can still send its reply
+            try:
+                self.target_sock.shutdown(socket.SHUT_WR)
+            except socket.error:
+                pass
+        self.link.finish(abort)
 
 
 class RecvEncrypt(threading.Thread):
 
-    def __init__(self, source_sock, target_sock, key):
+    def __init__(self, source_sock, target_sock, key, link, source_addr, target_addr):
         super(RecvEncrypt, self).__init__()
         self.source_sock = source_sock
         self.target_sock = target_sock
         self.key = key
-        self.source_addr = self.source_sock.getpeername()
-        self.target_addr = self.target_sock.getpeername()
+        self.link = link
+        self.source_addr = source_addr
+        self.target_addr = target_addr
 
     def run(self):
+        abort = False
         while True:
             try:
                 data = recvtlv(self.source_sock)
-                if not data:
+                if data is None:
                     break
                 logger.debug('read  %04i from %s:%d', len(data) + 3, self.source_addr[0], self.source_addr[1])
-                sent = self.target_sock.send(decrypt(data, self.key))
-                logger.debug('write %04i to   %s:%d', sent, self.target_addr[0], self.target_addr[1])
+                if not data:
+                    continue
+                self.target_sock.sendall(decrypt(data, self.key))
+                logger.debug('write %04i to   %s:%d', len(data), self.target_addr[0], self.target_addr[1])
             except socket.error as e:
                 logger.error('socket error, e: %s', e)
+                abort = True
                 break
             except Exception as e:
                 logger.error('unknown error, e: %s', e)
+                abort = True
                 break
         logger.debug('connection %s:%d is closed.', self.source_addr[0], self.source_addr[1])
-        try:
-            self.source_sock.shutdown(socket.SHUT_RDWR)
-            self.target_sock.shutdown(socket.SHUT_RDWR)
-        except socket.error as e:
-            pass
+        if not abort:
+            # propagate the half close so the peer can still send its reply
+            try:
+                self.target_sock.shutdown(socket.SHUT_WR)
+            except socket.error:
+                pass
+        self.link.finish(abort)
 
 
 class PyTunnel(object):
@@ -161,32 +207,66 @@ class PyTunnel(object):
 
     def run(self):
         while True:
-            source_sock, source_addr = self.sock.accept()
-            target_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
-                target_sock.connect((self.remote_ip, self.remote_port))
+                source_sock, source_addr = self.sock.accept()
             except socket.error as e:
-                source_sock.close()
-                target_sock.close()
+                logger.error('accept error, e: %s', e)
                 continue
+            try:
+                self.handle(source_sock, source_addr)
+            except Exception as e:
+                # never let a single bad connection kill the accept loop
+                logger.error('connection setup error, e: %s', e)
+                try:
+                    source_sock.close()
+                except socket.error:
+                    pass
 
-            if self.mode == 'server':
-                threads = [
-                    RecvEncrypt(source_sock, target_sock, self.key),
-                    SendEncrypt(target_sock, source_sock, self.key)
-                ]
-            elif self.mode == 'client':
-                threads = [
-                    SendEncrypt(source_sock, target_sock, self.key),
-                    RecvEncrypt(target_sock, source_sock, self.key)
-                ]
+    def handle(self, source_sock, source_addr):
+        target_addr = (self.remote_ip, self.remote_port)
+        target_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            target_sock.connect(target_addr)
+        except socket.error as e:
+            source_sock.close()
+            target_sock.close()
+            return
 
-            for t in threads:
-                t.setDaemon(True)
-                t.start()
+        link = Link(source_sock, target_sock)
+        if self.mode == 'server':
+            threads = [
+                RecvEncrypt(source_sock, target_sock, self.key, link, source_addr, target_addr),
+                SendEncrypt(target_sock, source_sock, self.key, link, target_addr, source_addr)
+            ]
+        elif self.mode == 'client':
+            threads = [
+                SendEncrypt(source_sock, target_sock, self.key, link, source_addr, target_addr),
+                RecvEncrypt(target_sock, source_sock, self.key, link, target_addr, source_addr)
+            ]
+        else:
+            source_sock.close()
+            target_sock.close()
+            raise ValueError('unknown mode: %s' % self.mode)
+
+        for t in threads:
+            t.setDaemon(True)
+            t.start()
 
     def __del__(self):
         self.sock.close()
+
+
+def parse_addr(addr):
+    """Parse an 'address:port' pair. Return None when it is missing or malformed."""
+    if not addr or addr.count(':') != 1:
+        return None
+    ip, port = addr.split(':')
+    if not ip or not port.isdigit():
+        return None
+    port = int(port)
+    if not 0 < port < 65536:
+        return None
+    return ip, port
 
 
 def main():
@@ -202,12 +282,15 @@ def main():
         parser.print_help()
         sys.exit()
 
+    local = parse_addr(opts.local_addr)
+    remote = parse_addr(opts.remote_addr)
+
     opts_error = False
     if opts.mode not in ('client', 'server'):
         opts_error = True
-    elif not (opts.key and len(opts.key) > 4):
+    if not opts.key:
         opts_error = True
-    elif ':' not in opts.local_addr or ':' not in opts.remote_addr:
+    if local is None or remote is None:
         opts_error = True
 
     if opts_error:
@@ -219,10 +302,8 @@ def main():
     else:
         logging.disable(logging.CRITICAL)
 
-    local_ip, local_port = opts.local_addr.split(':')
-    remote_ip, remote_port = opts.remote_addr.split(':')
-    local_port = int(local_port)
-    remote_port = int(remote_port)
+    local_ip, local_port = local
+    remote_ip, remote_port = remote
     key = hashlib.sha1(opts.key).hexdigest()
     tunnel = PyTunnel(local_ip, local_port, remote_ip, remote_port, opts.mode, key)
 
